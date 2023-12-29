fd620ed21e4a1d259bce7d3c3791c215ab974d9e
Zequn Fu <zequn.fu@amplitude.com>
2023-12-29 03:18:52 -0800

    AMP-91071 minor

--- unload_databricks_data_to_s3.py
+++ unload_databricks_data_to_s3.py
@@ -3,15 +3,15 @@ import sys
 
 def main():
     parser = argparse.ArgumentParser(description='unload data from databricks using SparkPython')
-    parser.add_argument("table_versions_map", help="tables and version ranges where data imported from. "
+    parser.add_argument("--table_versions_map", help="tables and version ranges where data imported from. "
                                                  "Shall be in format [{tableVersion},...,{tableVersion*N}]. "
                                                  "{tableVersion} will be '{catalogName}.{schemaName}.{tableName}={startingVersion}-{endingVersion}'")
-    parser.add_argument("data_type", help="type of data to be imported. Shall be 'EVENT', 'USER_PROPERTY' or 'GROUP_PROPERTY'")
-    parser.add_argument("sql", help="transformation sql")
-    parser.add_argument("aws_access_key_secret_key", help="databricks secret key name of aws_access_key")
-    parser.add_argument("aws_secret_key_secret_key", help="databricks secret key name of aws_secret_key")
-    parser.add_argument("aws_session_token_secret_key", help="databricks secret key name of aws_session_token")
-    parser.add_argument("s3_path", help="s3 path where data will be written into")
+    parser.add_argument("--data_type", help="type of data to be imported. Shall be 'EVENT', 'USER_PROPERTY' or 'GROUP_PROPERTY'")
+    parser.add_argument("--sql", help="transformation sql")
+    parser.add_argument("--aws_access_key_secret_key", help="databricks secret key name of aws_access_key")
+    parser.add_argument("--aws_secret_key_secret_key", help="databricks secret key name of aws_secret_key")
+    parser.add_argument("--asaws_session_token_secret_key", help="databricks secret key name of aws_session_token")
+    parser.add_argument("--s3_path", help="s3 path where data will be written into")
 
     args = parser.parse_args()
     print(parser.table_versions_map)
