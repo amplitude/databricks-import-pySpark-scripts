f07e3c6b25e67702ce87548f12b92b3f009910c2
Zequn Fu <zequn.fu@amplitude.com>
2023-12-29 01:24:11 -0800

    AMP-91071 minor

--- unload_databricks_data_to_s3.py
+++ unload_databricks_data_to_s3.py
@@ -1,24 +1,31 @@
 import argparse
+import sys
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='unload data from databricks using SparkPython')
-    parser.add_argument("table_versions_map", help="tables and version ranges where data imported from. "
-                                                 "Shall be in format [{tableVersion},...,{tableVersion*N}]. "
-                                                 "{tableVersion} will be '{catalogName}.{schemaName}.{tableName}={startingVersion}-{endingVersion}'")
-    parser.add_argument("data_type", help="type of data to be imported. Shall be 'EVENT', 'USER_PROPERTY' or 'GROUP_PROPERTY'")
-    parser.add_argument("sql", help="transformation sql")
-    parser.add_argument("aws_access_key_secret_key", help="databricks secret key name of aws_access_key")
-    parser.add_argument("aws_secret_key_secret_key", help="databricks secret key name of aws_secret_key")
-    parser.add_argument("aws_session_token_secret_key", help="databricks secret key name of aws_session_token")
-    parser.add_argument("s3_path", help="s3 path where data will be written into")
-
-    args = parser.parse_args()
-    print(parser.table_versions_map)
-    print(parser.data_type)
-    print(parser.sql)
-    print(parser.aws_access_key_secret_key)
-    print(parser.aws_secret_key_secret_key)
-    print(parser.aws_session_token_secret_key)
-    print(parser.s3_path)
+    # parser = argparse.ArgumentParser(description='unload data from databricks using SparkPython')
+    # parser.add_argument("table_versions_map", help="tables and version ranges where data imported from. "
+    #                                              "Shall be in format [{tableVersion},...,{tableVersion*N}]. "
+    #                                              "{tableVersion} will be '{catalogName}.{schemaName}.{tableName}={startingVersion}-{endingVersion}'")
+    # parser.add_argument("data_type", help="type of data to be imported. Shall be 'EVENT', 'USER_PROPERTY' or 'GROUP_PROPERTY'")
+    # parser.add_argument("sql", help="transformation sql")
+    # parser.add_argument("aws_access_key_secret_key", help="databricks secret key name of aws_access_key")
+    # parser.add_argument("aws_secret_key_secret_key", help="databricks secret key name of aws_secret_key")
+    # parser.add_argument("aws_session_token_secret_key", help="databricks secret key name of aws_session_token")
+    # parser.add_argument("s3_path", help="s3 path where data will be written into")
+    #
+    # args = parser.parse_args()
+    # print(parser.table_versions_map)
+    # print(parser.data_type)
+    # print(parser.sql)
+    # print(parser.aws_access_key_secret_key)
+    # print(parser.aws_secret_key_secret_key)
+    # print(parser.aws_session_token_secret_key)
+    # print(parser.s3_path)
+    print(sys.argv[1])
+    print(sys.argv[2])
+    print(sys.argv[3])
+    print(sys.argv[4])
+    print(sys.argv[5])
+    print(sys.argv[6])
     exit(0)
 
