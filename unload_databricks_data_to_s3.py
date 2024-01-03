170531dffc4dcf2eb8519a4806b2863311b17377
Zequn Fu <zequn.fu@amplitude.com>
2024-01-03 00:26:28 -0800

    AMP-91071 minor

--- unload_databricks_data_to_s3.py
+++ unload_databricks_data_to_s3.py
@@ -14,24 +14,24 @@ def parse_table_versions_map_arg(table_versions_map: str) -> dict[str, list[int]
     with version range [11,12].
     :return: table to version ranges map. Sample output: {'catalog.schema.table': [1,2]}
     """
-    d = collections.defaultdict(list)
+    dictionary = collections.defaultdict(list)
     table_and_versions_list = table_versions_map.split(",")
     for table_and_versions in table_and_versions_list:
-        table = table_and_versions.split("=")[0]
+        table_name = table_and_versions.split("=")[0]
         versions = table_and_versions.split("=")[1].split("-")
-        d[table].append(int(versions[0]))
-        d[table].append(int(versions[1]))
-    return d
+        dictionary[table_name].append(int(versions[0]))
+        dictionary[table_name].append(int(versions[1]))
+    return dictionary
 
 
-def build_temp_view_name(table: str) -> str:
+def build_temp_view_name(table_full_name: str) -> str:
     """
     Build temp view name for the table. Wrap table name with '`' to escape '.'. Append `epoch` so view name is very
     unlikely collapse with another table.
-    :param table: table name
+    :param table_full_name: table name
     :return: temp view name for the table
     """
-    return '`{table}.{epoch}`'.format(table=table, epoch=int(time.time()))
+    return '`{table}.{epoch}`'.format(table=table_full_name, epoch=int(time.time()))
 
 
 def build_sql_to_query_table_of_version(table_full_name: str, ending_version: int) -> str:
@@ -43,7 +43,7 @@ def build_sql_to_query_table_between_versions(table_full_name: str, starting_ver
         table=table_full_name, starting_version=starting_version, ending_version=ending_version)
 
 
-def pull_data(table_full_name: str, starting_version: int, ending_version: int) -> DataFrame:
+def query_data(table_full_name: str, starting_version: int, ending_version: int) -> DataFrame:
     if starting_version == 0:
         return spark.sql(build_sql_to_query_table_of_version(table_full_name, ending_version))
     else:
@@ -52,7 +52,6 @@ def pull_data(table_full_name: str, starting_version: int, ending_version: int) 
 
 
 if __name__ == '__main__':
-    spark = SparkSession.builder.getOrCreate()
     parser = argparse.ArgumentParser(description='unload data from databricks using SparkPython')
     parser.add_argument("table_versions_map", help="""tables and version ranges where data imported from. 
     Format syntax is '[{tableVersion},...,{tableVersion*N}]'. '{tableVersion}' will be 
@@ -62,27 +61,39 @@ if __name__ == '__main__':
                         help="""type of data to be imported. 
                         Valid values are ['EVENT', 'USER_PROPERTY', 'GROUP_PROPERTY'].""")
     parser.add_argument("sql", help="transformation sql")
+    parser.add_argument("secret_scope", help="databricks secret scope name")
     parser.add_argument("secret_key_name_for_aws_access_key", help="databricks secret key name of aws_access_key")
     parser.add_argument("secret_key_name_for_aws_secret_key", help="databricks secret key name of aws_secret_key")
     parser.add_argument("secret_key_name_for_aws_session_token", help="databricks secret key name of aws_session_token")
+    parser.add_argument("s3_region", help="s3 region")
     parser.add_argument("s3_path", help="s3 path where data will be written into")
 
     args = parser.parse_args()
 
-    # print(args.table_versions_map)
-    # print(args.data_type)
-    sql: str = args.sql
-    # print(args.secret_key_name_for_aws_access_key)
-    # print(args.secret_key_name_for_aws_secret_key)
-    # print(args.secret_key_name_for_aws_session_token)
-    # print(args.s3_path)
+    spark = SparkSession.builder.getOrCreate()
+    # setup s3 credentials for data export
+    aws_access_key = dbutils.secrets.get(scope=parser.secret_scope, key=parser.secret_key_name_for_aws_access_key)
+    aws_secret_key = dbutils.secrets.get(scope=parser.secret_scope, key=parser.secret_key_name_for_aws_secret_key)
+    aws_session_token = dbutils.secrets.get(scope=parser.secret_scope, key=parser.secret_key_name_for_aws_session_token)
+    spark.conf.set("fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.TemporaryAWSCredentialsProvider")
+    spark.conf.set("fs.s3a.access.key", aws_access_key)
+    spark.conf.set("fs.s3a.secret.key", aws_secret_key)
+    spark.conf.set("fs.s3a.session.token", aws_session_token)
+    spark.conf.set("fs.s3a.endpoint.region", args.s3_region)
 
+    sql: str = args.sql
+
+    # Build temp views
     table_to_import_version_range_map: dict[str, list[int]] = parse_table_versions_map_arg(args.table_versions_map)
     for table, import_version_range in table_to_import_version_range_map.items():
-        df: DataFrame = pull_data(table, import_version_range[0], import_version_range[1])
-        print(df.show())
+        data: DataFrame = query_data(table, import_version_range[0], import_version_range[1])
         view_name: str = build_temp_view_name(table)
-        df.createOrReplaceTempView(view_name)
+        data.createOrReplaceTempView(view_name)
+        # replace table name in sql to get prepared for sql transformation
         sql = sql.replace(table, view_name)
-        df2: DataFrame = spark.sql(sql)
-        print(df2.show())
+
+    export_data: DataFrame = spark.sql(sql)
+    export_data.write.mode("overwrite").json(args.s3_path)
+
+    # stop spark session
+    SparkSession.stop(spark)
