ed94b70cb760f2487ae53367bf859010c13f7347
Zequn Fu <zequn.fu@amplitude.com>
2024-01-02 19:06:46 -0800

    AMP-91071 pull data from tables

---
+++ test/unload_databricks_data_to_s3_tests.py
@@ -0,0 +1,13 @@
+import unittest
+from unload_databricks_data_to_s3 import parse_table_versions_map_arg
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_parse_table_version(self):
+        expected = {'catalog.schema.table': [100, 200]}
+        self.assertEqual(expected,
+                         parse_table_versions_map_arg("catalog.schema.table=100-200"))
+        expected = {'catalog.schema.table': [1, 12], 'catalog2.schema2.table2': [11, 12]}
+        self.assertEqual(expected,
+                         parse_table_versions_map_arg("catalog.schema.table=1-12,catalog2.schema2.table2=11-12"))
--- unload_databricks_data_to_s3.py
+++ unload_databricks_data_to_s3.py
@@ -1,27 +1,58 @@
 import argparse
+import collections
 import sys
+import spark
 
-def main():
+
+def parse_table_versions_map_arg(table_versions_map: str) -> dict[str, list[int]]:
+    d = collections.defaultdict(list)
+    table_and_versions_list = table_versions_map.split(",")
+    for table_and_versions in table_and_versions_list:
+        table = table_and_versions.split("=")[0]
+        versions = table_and_versions.split("=")[1].split("-")
+        d[table].append(int(versions[0]))
+        d[table].append(int(versions[1]))
+    return d
+
+
+def pull_data(table_full_name: str, starting_version: int, ending_version: int):
+    if starting_version == 0:
+        return spark.sql("select * from {table} VERSION AS OF {version}"
+                         .format(table=table_full_name, version=ending_version))
+    else:
+        # TODO: filter data
+        return spark.sql("select * from table_changes({table}, {starting_version}, {ending_version})"
+                         .format(table=table_full_name,starting_version=starting_version,ending_version=ending_version))
+
+
+def import_data():
     parser = argparse.ArgumentParser(description='unload data from databricks using SparkPython')
-    parser.add_argument("table_versions_map", help="tables and version ranges where data imported from. "
-                                                 "Shall be in format [{tableVersion},...,{tableVersion*N}]. "
-                                                 "{tableVersion} will be '{catalogName}.{schemaName}.{tableName}={startingVersion}-{endingVersion}'")
-    parser.add_argument("data_type", help="type of data to be imported. Shall be 'EVENT', 'USER_PROPERTY' or 'GROUP_PROPERTY'")
+    parser.add_argument("table_versions_map", help="""tables and version ranges where data imported from. 
+    Format syntax is '[{tableVersion},...,{tableVersion*N}]'. '{tableVersion}' will be 
+    '{catalogName}.{schemaName}.{tableName}={startingVersion}-{endingVersion}'. 
+    Example: catalog1.schema1.table1=0-12,catalog2.schema2.table2=10-100 """)
+    parser.add_argument("data_type",
+                        help="""type of data to be imported. 
+                        Valid values are ['EVENT', 'USER_PROPERTY', 'GROUP_PROPERTY'].""")
     parser.add_argument("sql", help="transformation sql")
-    parser.add_argument("aws_access_key_secret_key", help="databricks secret key name of aws_access_key")
-    parser.add_argument("aws_secret_key_secret_key", help="databricks secret key name of aws_secret_key")
-    parser.add_argument("aws_session_token_secret_key", help="databricks secret key name of aws_session_token")
+    parser.add_argument("secret_key_name_for_aws_access_key", help="databricks secret key name of aws_access_key")
+    parser.add_argument("secret_key_name_for_aws_secret_key", help="databricks secret key name of aws_secret_key")
+    parser.add_argument("secret_key_name_for_aws_session_token", help="databricks secret key name of aws_session_token")
     parser.add_argument("s3_path", help="s3 path where data will be written into")
 
     args = parser.parse_args()
-    print(args.table_versions_map)
-    print(args.data_type)
-    print(args.sql)
-    print(args.aws_access_key_secret_key)
-    print(args.aws_secret_key_secret_key)
-    print(args.aws_session_token_secret_key)
-    print(args.s3_path)
+    table_to_import_version_range_map = parse_table_versions_map_arg(args.table_versions_map)
+    for table, import_version_range in table_to_import_version_range_map:
+        df = pull_data(table, import_version_range[0], import_version_range[1])
+        df.show()
+    # print(args.table_versions_map)
+    # print(args.data_type)
+    # print(args.sql)
+    # print(args.secret_key_name_for_aws_access_key)
+    # print(args.secret_key_name_for_aws_secret_key)
+    # print(args.secret_key_name_for_aws_session_token)
+    # print(args.s3_path)
 
 
 if __name__ == '__main__':
-    main();
+    import_data();
