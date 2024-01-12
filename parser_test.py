e6d2cb0566e7573820508124b5ce1e43d99ea07c
Zequn Fu <zequn.fu@amplitude.com>
2024-01-12 14:25:33 -0800

    AMP-91849 add script to test parser

---
+++ parser_test.py
@@ -0,0 +1,35 @@
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='unload data from databricks using SparkPython')
+    parser.add_argument("--table_versions_map", nargs='?', default=None,
+                        help="""tables and version ranges where data imported from.
+    Format syntax is '[{tableVersion},...,{tableVersion*N}]'. '{tableVersion}' will be
+    '{catalogName}.{schemaName}.{tableName}={startingVersion}-{endingVersion}'.
+    Example: catalog1.schema1.table1=0-12,catalog2.schema2.table2=10-100 """)
+    parser.add_argument("--data_type", nargs='?', default='EVENT',
+                        choices=['EVENT', 'USER_PROPERTY', 'GROUP_PROPERTY'],
+                        help="""type of data to be imported.
+                        Valid values are ['EVENT', 'USER_PROPERTY', 'GROUP_PROPERTY'].""")
+    parser.add_argument("--sql", nargs='?', default=None, help="transformation sql")
+    parser.add_argument("--secret_scope", nargs='?', default=None, help="databricks secret scope name")
+    parser.add_argument("--secret_key_name_for_aws_access_key", nargs='?', default=None,
+                        help="databricks secret key name of aws_access_key")
+    parser.add_argument("--secret_key_name_for_aws_secret_key", nargs='?', default=None,
+                        help="databricks secret key name of aws_secret_key")
+    parser.add_argument("--secret_key_name_for_aws_session_token", nargs='?', default=None,
+                        help="databricks secret key name of aws_session_token")
+    parser.add_argument("--s3_region", nargs='?', default=None, help="s3 region")
+    parser.add_argument("--s3_path", nargs='?', default=None, help="s3 path where data will be written into")
+
+    args = parser.parse_args()
+
+    print(args.table_versions_map)
+    print(args.data_type)
+    print(args.sql)
+    print(args.secret_scope)
+    print(args.secret_key_name_for_aws_access_key)
+    print(args.secret_key_name_for_aws_secret_key)
+    print(args.secret_key_name_for_aws_session_token)
+    print(args.s3_region)
+    print(args.s3_path)
